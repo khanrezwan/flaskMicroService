2daf5ced2207a7df3b966d7cc021988a7b70656f
rezwan <rezwanislam@gmail.com>
2018-01-04 01:16:16 +0600

    pymongo working

---
+++ userService/userServiceApp/model/model.py
@@ -0,0 +1,8 @@
+
+class Foo(object):
+    value:str
+
+class User(object):
+    name:str
+    age:int
+    foo:Foo
--- userService/userServiceApp/restApi/view.py
+++ userService/userServiceApp/restApi/view.py
@@ -1,9 +1,10 @@
 from  flask import current_app as app
 from flask import jsonify
 from userServiceApp import userMicroService
-
+from init_flask_app import pyMongo
 @userMicroService.route("/", methods=['GET'])
 def ping():
+    pyMongo.db.tasks.insert_one({'dd':1})
     return jsonify({
         'status': 'success',
         'message': 'pong!'
