2cb3a7646b71e207d36056f6f2d48839879a728e
rezwan <rezwanislam@gmail.com>
2018-01-12 15:12:23 +0600

    Dockerize start

--- userService/config.py
+++ userService/config.py
@@ -5,37 +5,62 @@ class BaseConfig:
     JSON_AS_ASCII = False
     #JSONIFY_MIMETYPE = True
     SECRET_KEY = 'My Precious'
-    MONGO_HOST = 'localhost'
-    MONGO_PORT = 27017
-
     #MONGO_URI = 'mongodb://localhost:27017/userService'
     @staticmethod
     def init_app(app):
         pass
 
+class DevConfigDocker(BaseConfig):
+    """Development configuration for Docker"""
+    MONGO_HOST = 'userservice-mongo'
+    MONGO_PORT = 27017
+    DEBUG = True
+    MONGO_DBNAME = 'userServiceDev'
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
+    MONGO_HOST = 'localhost'
+    MONGO_PORT = 27017
     DEBUG = True
     MONGO_DBNAME = 'userServiceDev'
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
+    MONGO_HOST = 'localhost'
     DEBUG = True
     TESTING = True
     MONGO_DBNAME = 'userServiceTest'
 
+class TestingConfigDocker(BaseConfig):
+    """Testing configuration for Docker"""
+    MONGO_HOST = 'userservice-mongo'
+    MONGO_PORT = 27017
+    DEBUG = True
+    TESTING = True
+    MONGO_DBNAME = 'userServiceTest'
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
+    MONGO_HOST = 'localhost'
+    DEBUG = False
+    MONGO_DBNAME = 'userService'
+
+class ProductionConfigDocker(BaseConfig):
+    """Production configuration"""
+    MONGO_HOST = 'localhost'
+    MONGO_PORT = 27017
     DEBUG = False
     MONGO_DBNAME = 'userService'
 
 config_prefix = 'MONGO'
+
 config = {
     'development': DevelopmentConfig,
+    'developmentDocker': DevConfigDocker,
     'testing': TestingConfig,
+    'testingDocker':TestingConfigDocker,
     'production': ProductionConfig,
-    'default': DevelopmentConfig
+    'productionDocker':ProductionConfigDocker,
+    'default': DevConfigDocker
 }
